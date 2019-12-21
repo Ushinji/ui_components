4a0bfd176fd269d2a54320a9423a0e9abd4d8eb8
Ushinji <siguma971@gmail.com>
2019-12-21 13:12:23 +0900

    Fix page layout design.

--- frontend/components/molecules/Header/index.tsx
+++ frontend/components/molecules/Header/index.tsx
@@ -3,17 +3,23 @@ import styled from 'styled-components';
 import colors from '@frontend/components/styles/colors';
 
 const Container = styled.div`
-  padding: 8px;
-  text-align: left;
   border: 0;
   background-color: ${colors.white};
   box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.15);
   height: 40px;
+`;
+
+const Content = styled.div`
+  padding: 8px 24px;
   line-height: 24px;
 `;
 
 const Header: React.FC = () => {
-  return <Container>UI Compoents</Container>;
+  return (
+    <Container>
+      <Content>UI Compoents</Content>
+    </Container>
+  );
 };
 
 export default Header;
--- frontend/components/templates/HeaderTemplate/index.tsx
+++ frontend/components/templates/HeaderTemplate/index.tsx
@@ -3,7 +3,10 @@ import styled from 'styled-components';
 import Header from '@frontend/components/molecules/Header';
 
 const Container = styled.div``;
-const Body = styled.div``;
+const Body = styled.div`
+  width: 800px;
+  margin: 0 auto;
+`;
 
 type Props = {
   children: React.ReactNode;
