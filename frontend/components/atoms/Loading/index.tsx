1291a8d45c89f12b1051f07f8d1b619ad811bd2c
Ushinji <siguma971@gmail.com>
2019-12-24 00:08:40 +0900

    Fix height of Loading component

--- frontend/components/atoms/Loading/index.tsx
+++ frontend/components/atoms/Loading/index.tsx
@@ -4,7 +4,6 @@ import colors from '@frontend/components/styles/colors';
 
 const Container = styled.div`
   width: 100%;
-  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
