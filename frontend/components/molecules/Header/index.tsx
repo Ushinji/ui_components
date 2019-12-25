4f955ec35aef5be8036eeac3c35a38fd52273fab
Ushinji <siguma971@gmail.com>
2019-12-26 00:48:51 +0900

    Fix Header position.

--- frontend/components/molecules/Header/index.tsx
+++ frontend/components/molecules/Header/index.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import colors from '@frontend/components/styles/colors';
+import variables from '@frontend/components/styles/variables';
 
 const Container = styled.div`
+  position: fixed;
+  top: 0;
   border: 0;
   background-color: ${colors.white};
   box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.15);
-  height: 40px;
+  height: ${variables.headerHeight};
+  width: 100vw;
 `;
 
 const Content = styled.div`
--- frontend/components/templates/HeaderTemplate/index.tsx
+++ frontend/components/templates/HeaderTemplate/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import Header from '@frontend/components/molecules/Header';
+import variables from '@frontend/components/styles/variables';
 
 const Container = styled.div``;
 const Body = styled.div`
   width: 940px;
   margin: 0 auto;
+  padding-top: ${variables.headerHeight};
 `;
 
 type Props = {
